4f2190f1e8537d0bd27ad09ae56809a4829cfcc5
Dustin Reed Morado <dustin.morado@gmail.com>
2014-12-09 09:53:33 -0600

    Fixed Install script to handle paths w/ spaces

--- install_tomoauto.sh
+++ install_tomoauto.sh
@@ -8,25 +8,25 @@
 # Written: December 2nd 2014                                                   #
 # Contact: Dustin dot Morado at gmail                                          #
 #------------------------------------------------------------------------------#
-printf "Hello $USER!\n"
+printf "Hello \"$USER\"!\n"
 printf "This shell script handles the installation of tomoauto\n"
 printf "======================================================\n\n"
 printf "Where is tomoauto currently located?\n" 
-printf "\t(Default: $PWD): "
+printf "\t(Default: \"$PWD\"): "
 read tomoauto_answer
 
-if [ $tomoauto_answer ]; then
-    tomoauto_dir=$tomoauto_answer
+if [ "$tomoauto_answer" ]; then
+    tomoauto_dir="$tomoauto_answer"
 else
-    tomoauto_dir=$PWD
+    tomoauto_dir="$PWD"
 fi
 
-printf "\nInstalling tomoauto to: $tomoauto_dir\n\n"
+printf "\nInstalling tomoauto to: \"$tomoauto_dir\"\n\n"
 
 printf "Do you want to proceed with the installation? [Y/N]: "
 read user_approval
 
-case $user_approval in
+case "$user_approval" in
     Y|y)
         printf "Proceeding...\n\n"
         ;;
@@ -41,10 +41,10 @@ case $user_approval in
         ;;
 esac
 
-user_shell=$(basename $SHELL)
-tomoauto_init_file=$tomoauto_dir/tomoauto_init.$user_shell
-export TOMOAUTOROOT=$tomoauto_dir
-lua_dir=$tomoauto_dir/lua
+user_shell=$(basename "$SHELL")
+tomoauto_init_file="$tomoauto_dir"/tomoauto_init."$user_shell"
+export TOMOAUTOROOT="$(echo "$tomoauto_dir" | sed 's/ /\\ /g')"
+lua_dir="$tomoauto_dir"/lua
 lua_install="lua-5.2.3"
 lfs_install="luafilesystem-1.6.2"
 struct_install="struct-0.2"
@@ -63,61 +63,65 @@ case $(uname) in
         ;;
 esac
 
-if [ ! -w $tomoauto_dir ]; then
-    printf "Error: You don't have write permissions for $tomoauto_dir\n"
+if [ ! -w "$tomoauto_dir" ]; then
+    printf "Error: You don't have write permissions for \"$tomoauto_dir\"\n"
     printf "Aborting installation... Goodbye!\n\n"
 fi
 
-tar xvJf $lua_dir/$lua_install.tar.xz --directory $lua_dir
-cd $lua_dir/$lua_install
-make $platform | tee $lua_dir/lua_install.log
-make install | tee -a $lua_dir/lua_install.log
+tar xvJf "$lua_dir"/"$lua_install".tar.xz --directory "$lua_dir"
+cd "$lua_dir"/"$lua_install"
+make "$platform" | tee "$lua_dir"/lua_install.log
+make install | tee -a "$lua_dir"/lua_install.log
 cd -
-ln -s $tomoauto_dir/lua/bin/lua $tomoauto_dir/bin/talua
+ln -s "$tomoauto_dir"/lua/bin/lua "$tomoauto_dir"/bin/talua
 
-tar xvJf $lua_dir/$lfs_install.tar.xz --directory $lua_dir
-cd $lua_dir/$lfs_install
+tar xvJf "$lua_dir"/"$lfs_install".tar.xz --directory "$lua_dir"
+cd "$lua_dir"/"$lfs_install"
 awk -v platform=${platform} \
     '$0 ~ platform { sub(/#/, ""); print } \
     $0 !~ platform { print }' config > config.new
 mv config config.bak && mv config.new config
-make | tee $lua_dir/lfs_install.log
-make install | tee -a $lua_dir/lfs_install.log
+make | tee "$lua_dir"/lfs_install.log
+make install | tee -a "$lua_dir"/lfs_install.log
 cd -
 
-tar xvJf $lua_dir/$struct_install.tar.xz --directory $lua_dir
-cd $lua_dir/$struct_install
+tar xvJf "$lua_dir"/"$struct_install".tar.xz --directory "$lua_dir"
+cd "$lua_dir"/"$struct_install"
 awk -v platform=${platform} \
     '$0 ~ platform { sub(/#/, ""); print } \
     $0 !~ platform { print }' makefile > makefile.new
 mv makefile makefile.bak && mv makefile.new makefile
-make | tee $lua_dir/struct_install.log
-make install | tee -a $lua_dir/struct_install.log
+make | tee "$lua_dir"/struct_install.log
+make install | tee -a "$lua_dir"/struct_install.log
 cd -
 
-if [ -f $tomoauto_init_file ]; then
-    mv $tomoauto_init_file $tomoauto_init_file.bak
+if [ -f "$tomoauto_init_file" ]; then
+    mv "$tomoauto_init_file" "$tomoauto_init_file".bak
 fi
 
-touch $tomoauto_init_file
-case $user_shell in
+touch "$tomoauto_init_file"
+case "$user_shell" in
     sh|bash|zsh|ksh)
-        printf "export TOMOAUTOROOT=$tomoauto_dir\n" >> $tomoauto_init_file
-        printf "export PATH=$tomoauto_dir/bin:\$PATH\n" >> $tomoauto_init_file
+        printf "export TOMOAUTOROOT=\"$tomoauto_dir\"\n" >> \
+            "$tomoauto_init_file"
+        printf "export PATH=\"$tomoauto_dir/bin:$PATH\"\n" >> \
+            "$tomoauto_init_file"
         printf "export LUA_PATH=\"$tomoauto_dir/lib/?.lua;;\"\n" >> \
-            $tomoauto_init_file
+            "$tomoauto_init_file"
         printf "export LUA_CPATH=\"$tomoauto_dir/lua/lib/lua/5.2/?.so;;\"\n">> \
-            $tomoauto_init_file
+            "$tomoauto_init_file"
         ;;
     csh|tcsh)
-        printf "setenv TOMOAUTOROOT $tomoauto_dir\n" >> $tomoauto_init_file
-        printf "setenv PATH $tomoauto_dir/bin:\$PATH\n" >> $tomoauto_init_file
+        printf "setenv TOMOAUTOROOT \"$tomoauto_dir\"\n" >> \
+            "$tomoauto_init_file"
+        printf "setenv PATH \"$tomoauto_dir/bin:$PATH\"\n" >> \
+            "$tomoauto_init_file"
         printf "setenv LUA_PATH=\"$tomoauto_dir/lib/?.lua;;\"\n" >> \
-            $tomoauto_init_file
+            "$tomoauto_init_file"
         printf "setenv LUA_CPATH=\"$tomoauto_dir/lua/lib/lua/5.2/?.so;;\"\n">> \
-            $tomoauto_init_file
+            "$tomoauto_init_file"
         ;;
 esac
 
 printf "Installation complete! "
-printf "Source $tomoauto_init_file in your shell rc file if you'd like\n"
+printf "Source \"$tomoauto_init_file\" in your shell rc file if you'd like\n"
